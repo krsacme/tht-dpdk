e5ab336ae21e3e261d4b16f70872b082dc58fea7
Saravanan KR <skramaja@redhat.com>
2016-11-25 05:41:09 -0500

    Setting up of vxlan for tenant network on separate bridge

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 openstack overcloud deploy --templates \
     --timeout 180 \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /home/stack/templates/network-environment.yaml
 
+#    -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
