2f113d1e2ff3c9049ca0bae113f90855786816b3
Your Name <you@example.com>
2016-12-13 06:16:46 -0500

    VxLan on Flat is working
    * Added puppet-vswitch for restart openvswitch - https://review.openstack.org/#/c/409779
    * All nodes validation disabled for ping test failure
    * br-link does not retain IP, requires reboot of compute node after deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 openstack overcloud deploy --templates \
     --timeout 180 \
+    -e /home/stack/templates/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /home/stack/templates/network-environment.yaml
 
+#    -e /home/stack/templates/network-isolation.yaml \
+#/usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
