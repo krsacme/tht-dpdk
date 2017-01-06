4f3a01cb9e6de67f70d746f8bc4797b69af25e68
Your Name <you@example.com>
2017-01-06 01:59:21 -0500

    gre with network isolation - DPDK as tenant network

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 openstack overcloud deploy --templates \
     --timeout 180 \
-    -e /home/stack/templates/network-isolation.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /home/stack/templates/network-environment.yaml
 
+#openstack overcloud deploy --templates \
+#    --timeout 180 \
+#    -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+#    -e /home/stack/templates/network-environment.yaml \
+#    -e motd-environment.yaml -r roles_data.yaml
 #    -e /home/stack/templates/network-isolation.yaml \
+#
+#    -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
 #/usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
