6b9ee145ed5e69fafe87cd0059e85c0967858889
Saravanan KR <skramaja@redhat.com>
2017-03-09 18:18:01 +0530

    OvS 2.6 deployment scripts for fresh and update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 openstack overcloud deploy --templates \
     --timeout 90 \
-    -r /home/stack/templates/roles_data.yaml \
+    -r roles_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-    -e /home/stack/templates/network-environment.yaml
+    -e network-environment.yaml
 
--- plan_update.sh
+++ plan_update.sh
@@ -1,8 +1,8 @@
 openstack overcloud deploy --templates \
     --update-plan-only  \
     --timeout 90 \
-    -r /home/stack/templates/roles_data.yaml \
+    -r roles_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-    -e /home/stack/templates/network-environment.yaml
+    -e network-environment.yaml
 
