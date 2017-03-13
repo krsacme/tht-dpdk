10b8f16c044bc117c6dc0afb75b20612683a37c9
Saravanan KR <skramaja@redhat.com>
2017-03-13 15:19:29 +0530

    Deploy and Update post install scripts separated

--- plan_update_ovs_2-5_to_2-6.sh
+++ plan_update_ovs_2-5_to_2-6.sh
@@ -4,5 +4,5 @@ openstack overcloud deploy --templates \
     -r roles_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-    -e network-environment.yaml
+    -e network-environment-update.yaml
 
