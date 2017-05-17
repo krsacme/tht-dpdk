aab54d8eea81253e52ec6dfcbc146fe8141c20f8
karthiksundaravel <ksundara@redhat.com>
2017-05-17 21:12:19 +0530

    Update upgrade_step3_converge.sh
    Removed custom roles data

--- osp10_ovs25_upgradeto_11/upgrade_step3_converge.sh
+++ osp10_ovs25_upgradeto_11/upgrade_step3_converge.sh
@@ -1,6 +1,5 @@
 openstack overcloud deploy --templates \
     --timeout 90 \
-    -r roles_data_osp11.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e network-environment-update.yaml \
