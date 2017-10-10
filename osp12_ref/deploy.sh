b6eef9cdaf373cb85cdb56eeed42c0b2e5754d44
Saravanan KR <skramaja@redhat.com>
2017-10-10 11:38:30 +0530

    Restrucuture osp12 to accomodate ml2 ovs and odl

--- osp12_ref/deploy.sh
+++ osp12_ref/deploy.sh
@@ -4,19 +4,18 @@ source base.sh
 
 openstack overcloud deploy \
     --templates \
-    -r roles_data.yaml \
+    -r common/roles_data.yaml \
     --timeout 90 \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
-    -e registry.yaml \
-    -e dpdk-environment.yaml \
-    -e common-environment.yaml \
-    -e $ENV_FILE \
-    -e docker_registry.yaml \
-    -e temp-env.yaml
+    -e common/registry.yaml \
+    -e ml2-ovs-dpdk-env.yaml \
+    -e common/environment.yaml \
+    -e common/$ENV_FILE \
+    -e docker_registry.yaml 
 
 # Post Deploy Workarounds
 # systemctl start virtlogd.socket
