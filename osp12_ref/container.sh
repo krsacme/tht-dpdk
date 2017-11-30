6f5270b1c51b680e2482fb44173aa5436eb55f18
Saravanan KR <skramaja@redhat.com>
2017-11-30 17:52:13 +0530

    Updated OSP12 templates with derived parameters execution

--- osp12_ref/container.sh
+++ osp12_ref/container.sh
@@ -5,7 +5,8 @@ openstack overcloud container image prepare   \
   --prefix "openstack-" \
   --suffix "docker" \
   --tag latest \
-  --env-file docker_registry.yaml
+  --env-file docker_registry.yaml \
+  --service-environment-file /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml
 
 #sed -i 's/docker:/-docker:/g' docker_registry.yaml
 echo "  DockerInsecureRegistryAddress: 10.60.19.51:5000" >> docker_registry.yaml
