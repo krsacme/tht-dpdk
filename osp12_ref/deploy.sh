2ab69eb1d1b24ca2c33a9997c19883f158d1706e
Saravanan KR <skramaja@redhat.com>
2017-11-30 17:52:13 +0530

    Updated OSP12 templates with derived parameters execution

--- osp12_ref/deploy.sh
+++ osp12_ref/deploy.sh
@@ -5,17 +5,21 @@ source base.sh
 openstack overcloud deploy \
     --templates \
     -r common/roles_data.yaml \
+    -p /usr/share/openstack-tripleo-heat-templates/plan-samples/plan-environment-derived-params.yaml \
     --timeout 90 \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
-    -e common/registry.yaml \
-    -e ml2-ovs-dpdk-env.yaml \
+    -e registry.yaml \
     -e common/environment.yaml \
     -e common/$ENV_FILE \
-    -e docker_registry.yaml 
+    -e docker_registry.yaml
+
+
+# Not required with derived parameters workflow
+#    -e ml2-ovs-dpdk-env.yaml \
 
 # Post Deploy Workarounds
 # systemctl start virtlogd.socket
