fed0f024b9416bc2f29470062814a635f1be63b4
Saravanan KR <skramaja@redhat.com>
2018-05-07 21:11:21 +0530

    OSP10 with OvS2.9 fresh deployment templates added

---
+++ osp10_ovs29/deploy.sh
@@ -0,0 +1,8 @@
+openstack overcloud deploy --templates \
+    --timeout 90 \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+    -e network-environment.yaml \
+    -e ovs29.yaml
+
+
