8b5b360880a4f16e6eadb25bd0b121554c40c51d
Saravanan KR <skramaja@redhat.com>
2018-05-09 18:30:56 +0530

    Fixed OSP13 templates for the new lab

--- osp13_ref/container.sh
+++ osp13_ref/container.sh
@@ -2,21 +2,9 @@
 
 source base.sh
 
-if [[ $LAB == '' ]]; then 
-  openstack overcloud container image prepare   \
-    --namespace 10.60.19.51:5000/rhosp13  \
-    --prefix "openstack-" \
-    --tag latest \
-    --service-environment-file /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
-    --env-file docker_registry.yaml
-
-else
-
-  openstack overcloud container image prepare   \
-    --namespace docker-registry.engineering.redhat.com/rhosp13  \
-    --prefix "openstack-" \
-    --tag 2018-03-16.1 \
-    --service-environment-file /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
-    --env-file docker_registry.yaml
-fi
+openstack overcloud container image prepare   \
+  --namespace docker-registry.engineering.redhat.com/rhosp13  \
+  --prefix "openstack-" \
+  --tag 2018-05-07.2 \
+  --env-file docker_registry.yaml
 
--- osp13_ref/deploy.sh
+++ osp13_ref/deploy.sh
@@ -2,13 +2,15 @@
 
 source base.sh
 
+if [[ -f 'common/roles_data.yaml' ]]; then
+  openstack overcloud role generate -o common/roles_data.yaml Controller ComputeOvsDpdk
+fi
+
 openstack overcloud deploy \
     --templates \
     --timeout 120 \
     -r common/roles_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk-permissions.yaml \
@@ -18,12 +20,3 @@ openstack overcloud deploy \
     -e docker_registry.yaml \
     -e ml2-ovs-dpdk-env.yaml
 
-
-#    -e /usr/share/openstack-tripleo-heat-templates/environments/config-download-environment.yaml \
-#    -e /usr/share/openstack-tripleo-heat-templates/environments/nonha-arch.yaml \
-#    -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
-#    -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
-#    -e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/neutron-ovs-dpdk.yaml \
-#    -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk-permissions.yaml \
-#    -r common/roles_data.yaml \
-#    -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
