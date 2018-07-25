95615ee82d547b01908a2cb690b84ff69d72d62f
Saravanan KR <skramaja@redhat.com>
2018-07-25 12:10:02 +0530

    Enabled tuned as the kernel fix is avaiable

--- osp14_ref/container.sh
+++ osp14_ref/container.sh
@@ -2,9 +2,14 @@
 
 source base.sh
 
+if [[ ! -f 'common/roles_data.yaml' ]]; then
+  openstack overcloud roles generate -o common/roles_data.yaml Controller ComputeOvsDpdk
+fi
+
 openstack overcloud container image prepare   \
   --namespace docker-registry.engineering.redhat.com/rhosp14  \
+  -r common/roles_data.yaml \
   --prefix "openstack-" \
-  --tag 2018-06-04.1 \
+  --tag 2018-07-09.1 \
   --env-file docker_registry.yaml
 
