fb26f5266aaef2633d3dd17c7488577c59c9f869
Saravanan KR <skramaja@redhat.com>
2018-09-05 18:51:04 +0530

    OSP10: Update templates to hybrid setup

--- osp10_ovs29/overcloud_deploy.sh
+++ osp10_ovs29/overcloud_deploy.sh
@@ -1,10 +1,15 @@
+
+cd ~/osp10_ovs29/
+python generate.py -r roles_data.yaml -i first-boot.role.j2.yaml
+python generate.py -r roles_data.yaml -i dpdk-registry.j2.yaml
+
 openstack overcloud deploy --templates \
     --timeout 90 \
-    -r roles_data.yaml \
+    -r ~/osp10_ovs29/roles_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/ovs-dpdk-permissions.yaml \
-    -e dpdk-registry.yaml \
-    -e network-environment.yaml
+    -e ~/osp10_ovs29/dpdk-registry.yaml \
+    -e ~/osp10_ovs29/network-environment.yaml
 
 
