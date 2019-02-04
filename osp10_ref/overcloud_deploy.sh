da28df7615b68d1eb110785abd60525530ffa248
Saravanan KR <skramaja@redhat.com>
2019-02-04 10:19:17 +0530

    OSP10: Updated new cluster configs

--- osp10_ref/overcloud_deploy.sh
+++ osp10_ref/overcloud_deploy.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 
-cd ~/osp10_ovs29/
+cd ~/osp10_ref/
 python generate.py -r roles_data.yaml -i first-boot.role.j2.yaml
 python generate.py -r roles_data.yaml -i dpdk-registry.j2.yaml
 
 openstack overcloud deploy --templates \
     --timeout 90 \
-    -r ~/osp10_ovs29/roles_data.yaml \
+    -r ~/osp10_ref/roles_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/ovs-dpdk-permissions.yaml \
-    -e ~/osp10_ovs29/dpdk-registry.yaml \
-    -e ~/osp10_ovs29/network-environment.yaml
+    -e ~/osp10_ref/dpdk-registry.yaml \
+    -e ~/osp10_ref/network-environment.yaml
 
 
