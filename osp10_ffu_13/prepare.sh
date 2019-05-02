9482fc72b529d8ac2812f9d0532161f149b97f2f
Saravanan KR <skramaja@redhat.com>
2019-05-02 18:59:13 +0530

    FFU scripts updated

--- osp10_ffu_13/prepare.sh
+++ osp10_ffu_13/prepare.sh
@@ -2,10 +2,9 @@
 
 openstack overcloud ffwd-upgrade prepare --templates --yes \
     --timeout 90 \
-    -r ~/osp10_ref/roles_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
-    -e ~/osp10_ref/network-environment.yaml \
-    -e ~/osp10_ref/ffu_repo_env.yaml \
+    -e ~/osp10_ffu_13/network-environment.yaml \
+    -e ~/osp10_ffu_13/repo-env.yaml \
     -e ~/overcloud_images.yaml
