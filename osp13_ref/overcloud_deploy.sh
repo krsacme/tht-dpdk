157a2a3429203138a1c147eedd432d74778bf97a
Saravanan KR <skramaja@redhat.com>
2019-06-21 10:14:24 +0530

    OSP13: rdu-nfv-lab: Ctl plane and OS Api in same interface with vlan isolation

--- osp13_ref/overcloud_deploy.sh
+++ osp13_ref/overcloud_deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 if [[ ! -f 'roles_data.yaml' ]]; then
-  openstack overcloud roles generate -o ~/roles_data.yaml Controller ComputeOvsDpdk
+  openstack overcloud roles generate -o ~/roles_data.yaml Controller ComputeOvsDpdkSriov
 fi
 
 openstack overcloud deploy --templates \
     --timeout 90 \
     -r ~/roles_data.yaml \
+    -n ~/osp13_ref/network_data.yaml \
+    -p /usr/share/openstack-tripleo-heat-templates/plan-samples/plan-environment-derived-params.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/neutron-ovs-dpdk.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs-dpdk.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/ovs-dpdk-permissions.yaml \
-    -e ~/osp13_ref/network-environment.yaml \
-    -e ~/osp13_ref/docker-images.yaml \
-    -e ~/osp13_ref/ml2-ovs-dpdk-env.yaml
+    -e ~/osp13_ref/environment.yaml \
+    -e ~/osp13_ref/docker-images.yaml
