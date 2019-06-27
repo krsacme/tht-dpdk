5f1d53d22054ed7e69dde2e4c1f2c81e983c8c7f
Saravanan KR <skramaja@redhat.com>
2019-06-27 11:30:49 +0530

    Added offload templates and moved older templates to backup folder

--- osp14_ovn/overcloud_deploy.sh
+++ osp14_ovn/overcloud_deploy.sh
@@ -7,14 +7,16 @@ openstack overcloud deploy $PARAMS \
     --timeout 120 \
     -r ~/osp14_ref/roles_data.yaml \
     -n ~/osp14_ref/network_data_routed_pool3.yaml \
-    -p /usr/share/openstack-tripleo-heat-templates/plan-samples/plan-environment-derived-params.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-ha.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
     -e ~/osp14_ref/environment.yaml \
     -e ~/osp14_ref/network-environment.yaml \
-    -e ~/containers-prepare-parameter.yaml
+    -e ~/osp14_ref/ml2-ovs-dpdk-env.yaml \
+    -e ~/containers-prepare-parameter.yaml \
+    -e ~/override-images.yaml
 
 #    -e ~/osp14_ref/routed-environment.yaml \
