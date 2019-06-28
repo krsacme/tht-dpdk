aa245385b32e93d77ece4464873b255c7197d21f
Saravanan KR <skramaja@redhat.com>
2019-06-28 10:30:21 +0530

    OSP13: Fix error in offload tempalte's deploy command

--- offload/osp13_mlxoffload_vxlan/overcloud_deploy.sh
+++ offload/osp13_mlxoffload_vxlan/overcloud_deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+PARAMS="$*"
 USER_THT='/home/stack/ospd-13-vxlan-dpdk-sriov-ctlplane-dataplane-bonding-hybrid'
 DEFAULT_THT='/usr/share/openstack-tripleo-heat-templates'
 
-openstack overcloud deploy \
+openstack overcloud deploy $PARAMS\
   --templates \
   -r $USER_THT/roles_data.yaml \
   -n $USER_THT/network-data.yaml \
@@ -12,7 +13,7 @@ openstack overcloud deploy \
   -e $DEFAULT_THT/environments/host-config-and-reboot.yaml \
   -e $DEFAULT_THT/environments/services/neutron-ovs-dpdk.yaml \
   -e $DEFAULT_THT/environments/services/neutron-sriov.yaml \
-  -e $DEFAULT_THT/environments/services/ovs-hw-offload.yaml \
+  -e $DEFAULT_THT/environments/ovs-hw-offload.yaml \
   -e $USER_THT/docker-images.yaml \
   -e $USER_THT/api-policies.yaml \
   -e $USER_THT/os-net-config-mappings.yaml \
