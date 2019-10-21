740402daf62357abfddb80bb3fc6438660fbb800
Saravanan KR <skramaja@redhat.com>
2019-10-21 15:50:37 +0530

    OSP16: Reference templates updated

--- osp16_ref/overcloud_deploy.sh
+++ osp16_ref/overcloud_deploy.sh
@@ -12,12 +12,12 @@ openstack overcloud deploy $PARAMS \
     --timeout 120 \
     -r $HOME/roles_data.yaml \
     -n $USER_THT/network_data_routed_pool3.yaml \
-    -p /usr/share/openstack-tripleo-heat-templates/plan-samples/plan-environment-derived-params.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs-dpdk.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
     -e $USER_THT/environment.yaml \
     -e $USER_THT/network-environment.yaml \
     -e $USER_THT/ml2-ovs-dpdk.yaml \
