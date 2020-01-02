5459c5853bca44e26d41776e0b232d3ab201b9ef
Saravanan KR <skramaja@redhat.com>
2020-01-02 10:26:12 +0530

    OSP13: Aligned with perf requirements

--- osp13_ref/overcloud_deploy.sh
+++ osp13_ref/overcloud_deploy.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+PARAMS="$*"
+USER_THT="$HOME/osp13_ref"
+
 if [[ ! -f 'roles_data.yaml' ]]; then
-  openstack overcloud roles generate -o ~/roles_data.yaml Controller ComputeOvsDpdkSriov
+	openstack overcloud roles generate -o $HOME/roles_data.yaml Controller ComputeOvsDpdkSriov ComputeSriov
 fi
 
-openstack overcloud deploy --templates \
-    --timeout 90 \
-    -r ~/roles_data.yaml \
-    -n ~/osp13_ref/network_data.yaml \
-    -p /usr/share/openstack-tripleo-heat-templates/plan-samples/plan-environment-derived-params.yaml \
+openstack overcloud deploy $PARAMS \
+    --templates \
+    --timeout 120 \
+    -r $HOME/roles_data.yaml \
+    -n $USER_THT/network_data_routed_pool3.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs-dpdk.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/netcontrold.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
-    -e ~/osp13_ref/environment.yaml \
-    -e ~/osp13_ref/docker-images.yaml
+    -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
+    -e $USER_THT/environment.yaml \
+    -e $USER_THT/network-environment.yaml \
+    -e $USER_THT/ml2-ovs-dpdk.yaml
+
