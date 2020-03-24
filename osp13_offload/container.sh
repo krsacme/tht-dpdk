97dc039c8f492d3d4009b9a645d59b93b1712c9f
Saravanan KR <skramaja@redhat.com>
2020-03-24 16:11:15 +0530

    Rearranged offload tempaltes and moved older template to backup

---
+++ osp13_offload/container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+openstack overcloud container image prepare   \
+  --namespace docker-registry.engineering.redhat.com/rhosp13  \
+  --prefix "openstack-" \
+  --tag 2018-05-07.2 \
+  --env-file docker_registry.yaml
+
