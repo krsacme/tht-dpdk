d8351975c8b0462d558ca814a457df3c900b4890
Saravanan KR <skramaja@redhat.com>
2020-07-18 10:05:32 +0530

    OSP16: nic partition and offload templates added

--- osp16_ref/overcloud_deploy_offload.sh
+++ osp16_ref/overcloud_deploy_offload.sh
@@ -4,7 +4,7 @@ PARAMS="$*"
 USER_THT="$HOME/osp16_ref"
 
 if [[ ! -f 'roles_data.yaml' ]]; then
-	openstack overcloud roles generate -o $HOME/roles_data.yaml Controller ComputeOvsDpdkSriov
+	openstack overcloud roles generate -o $HOME/roles_data.yaml Controller ComputeSriov
 fi
 
 openstack overcloud deploy $PARAMS \
@@ -15,12 +15,11 @@ openstack overcloud deploy $PARAMS \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs-dpdk.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/services/netcontrold.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/ovs-hw-offload.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
     -e $USER_THT/environment.yaml \
-    -e $USER_THT/network-environment.yaml \
+    -e $USER_THT/network-environment-offload.yaml \
     -e $USER_THT/ml2-ovs-nfv.yaml \
     -e $HOME/containers-prepare-parameter.yaml
 
