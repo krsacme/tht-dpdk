9bd765a6b4b724be255f107596a4d49e7480e662
Saravanan KR <skramaja@redhat.com>
2020-07-27 13:46:18 +0530

    OSP13: Template fixed for brq lab

--- osp13_ref/overcloud_deploy.sh
+++ osp13_ref/overcloud_deploy.sh
@@ -3,23 +3,21 @@
 PARAMS="$*"
 USER_THT="$HOME/osp13_ref"
 
-if [[ ! -f 'roles_data.yaml' ]]; then
-	openstack overcloud roles generate -o $HOME/roles_data.yaml Controller ComputeOvsDpdkSriov ComputeSriov
-fi
+openstack overcloud roles generate -o $HOME/roles_data.yaml Controller ComputeOvsDpdkSriov
 
 openstack overcloud deploy $PARAMS \
     --templates \
     --timeout 120 \
     -r $HOME/roles_data.yaml \
-    -n $USER_THT/network_data_routed_pool3.yaml \
+    -n $USER_THT/network_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs-dpdk.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/services/netcontrold.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
     -e $USER_THT/environment.yaml \
     -e $USER_THT/network-environment.yaml \
-    -e $USER_THT/ml2-ovs-dpdk.yaml
+    -e $USER_THT/ml2-ovs-dpdk.yaml \
+    -e $USER_THT/docker-images.yaml
 
