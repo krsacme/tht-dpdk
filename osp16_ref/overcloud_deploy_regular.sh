90ddfe9fbe544fdb4de75fae7039546722b15602
Saravanan KR <skramaja@redhat.com>
2020-09-09 17:57:20 +0530

    OSP16: Fix overcloud deploy script

--- osp16_ref/overcloud_deploy_regular.sh
+++ osp16_ref/overcloud_deploy_regular.sh
@@ -14,7 +14,6 @@ openstack overcloud deploy $PARAMS \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs-dpdk.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/services/netcontrold.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
     -e $USER_THT/environment.yaml \
